bbc44772405f1e38884dae715de15a081c7afa7f
Mario Chavez <mariochavez1292@gmail.com>
2023-04-12 16:51:08 -0600

    rotation and movement to pices

--- game/Tetrimino.py
+++ game/Tetrimino.py
@@ -1,8 +1,12 @@
 import random
 import time
 import pygame
+import numpy as np
 
 class Tetrimino:
+
+
+
     s = [[1]]
 
     O = [[1, 1],
@@ -25,63 +29,97 @@ class Tetrimino:
          [0, 0, 0]]
 
     I = [[0, 0, 0, 0],
-         [0, 0, 0, 0],
          [1, 1, 1, 1],
+         [0, 0, 0, 0],
          [0, 0, 0, 0]]
 
     T = [[0, 1, 0],
          [1, 1, 1],
          [0, 0, 0]]
 
-    kinds = [I, J, L, O, S, T, Z]
+    kinds = [I, O, T, S, Z, J, L]
     colors = [
         (000, 255, 255, 255), # cyan
         (255, 255, 000, 255), # yellow
         (128, 000, 128, 255), # purple
-        (000, 128, 000, 255), # green
+        (000, 255, 000, 255), # green
         (255, 000, 000, 255), # red
         (000, 000, 255, 255), # blue
         (255, 165, 000, 255), # orange
         ]
 
     def __init__(self, square_size):
-        random_index = (random.randint(1, 100) * time.time_ns()) % len(Tetrimino.kinds)
-        self.shape = Tetrimino.kinds[random_index]
-        self.color = Tetrimino.colors[random_index]
-
-        self.shape = Tetrimino.s # Hardcode s to test game
-        self.color = (255, 255, 255, 255)
-
-        self.rects = list()
+        random_int = (random.randint(1, 100) * time.time_ns())
         self.square_size = square_size
+        self.shape_index = random_int % len(Tetrimino.kinds)
+        self.shape = Tetrimino.kinds[self.shape_index]
+        self.color = Tetrimino.colors[self.shape_index]
         self.min_x = 0
         self.min_y = 0
-        self.max_x = 0
-        self.max_y = 0
-        x, y = 0, 0
+        where = np.where(np.array(self.shape) == 1)
+        self.width = max(where[1]) + 1
+        self.height = max(where[0]) + 1
+        self.max_x = self.square_size * self.width
+        self.max_y = self.square_size * self.height
+        self.rects = self.define_rects()
+
+    def define_rects(self):
+        rects = list()
+        x, y = self.min_x, self.min_y
         for row in self.shape:
             for square in row:
                 if square == 1:
-                    self.rects.append(pygame.Rect(x, y, square_size, square_size))
-                x += square_size
-            y += square_size
+                    rects.append(pygame.Rect(x, y, self.square_size, self.square_size))
+                x += self.square_size
+            x = self.min_x
+            if 1 in row:
+                y += self.square_size
+        return rects
 
     def move_left(self, units = 1):
-        # check if is in game area
-        # update mins or maxs
+        total_move = units * self.square_size
+        self.min_x -= total_move
+        self.max_x -= total_move
         for square in self.rects:
-            square.x -= units * self.square_size
+            square.x -= total_move
 
     def move_right(self, units = 1):
-        # check if is in game area
+        total_move = units * self.square_size
+        self.min_x += total_move
+        self.max_x += total_move
         for square in self.rects:
-            square.x += units * self.square_size
+            square.x += total_move
 
     def move_down(self, units = 1):
-        # check if is in game area
+        total_move = units * self.square_size
+        self.min_y += total_move
+        self.max_y += total_move
         for square in self.rects:
-            square.y += units * self.square_size
+            square.y += total_move
+
+    def move_up(self, units = 1):
+        total_move = units * self.square_size
+        self.min_y -= total_move
+        self.max_y -= total_move
+        for square in self.rects:
+            square.y -= total_move
+
+    def rotate(self, times = 1):
+
+
+        self.shape = [[row[-i-1] for row in self.shape] for i in range(len(self.shape[0]))]
+        self.height = len(self.shape)
+        self.width = len(self.shape[0])
+        self.max_x = self.square_size * self.width
+        self.max_y = self.square_size * self.height
+        self.rects = self.define_rects()
 
     def draw(self, display: pygame.display):
         for rect in self.rects:
             pygame.draw.rect(display, self.color, rect)
+
+    def __str__(self):
+        # res = str()
+        # for row in self.shape:
+        #     res += str(row) + '\n'
+        return str(self.shape)
--- game/main.py
+++ game/main.py
@@ -3,11 +3,10 @@ import pygame
 from Tetrimino import Tetrimino
 
 SQUARE_SIZE = 40
-SCREEN = {'h': 1000, 'w': 400}
-GAME_SURF = pygame.Surface(GAME_SIZE := (400, 400))
+GAME_SURF = pygame.Surface(GAME_SIZE := (400, 800))
 # returns a surface the size of the screen
-DISPLAY_SURF = pygame.display.set_mode((SCREEN['w'], SCREEN['h']))
-FPS = 60
+DISPLAY_SURF = pygame.display.set_mode(DISPLAY_SIZE := (408, 900))
+FPS = 10
 
 def main():
     clock = pygame.time.Clock()
@@ -30,22 +29,25 @@ def draw_window(tetri: Tetrimino):
     GAME_SURF.fill(pygame.Color('black'))
     DISPLAY_SURF.fill(pygame.Color('gray'))
     tetri.draw(GAME_SURF)
-    DISPLAY_SURF.blit(GAME_SURF, (0, 100))
+    DISPLAY_SURF.blit(GAME_SURF, (4, 4))
     pygame.display.update()
 
 def tetri_movement(keys_pressed, tetri: Tetrimino):
 
-    if keys_pressed[pygame.K_a]: # and tetri.min_x - SQUARE_SIZE >= 0:
+    if keys_pressed[pygame.K_a] and tetri.min_x - SQUARE_SIZE >= 0:
         tetri.move_left()
 
-    if keys_pressed[pygame.K_d]: # and tetri.max_x + SQUARE_SIZE <= GAME_SIZE[0] - SQUARE_SIZE:
+    if keys_pressed[pygame.K_d] and tetri.max_x + SQUARE_SIZE <= GAME_SIZE[0] - SQUARE_SIZE * tetri.width:
         tetri.move_right()
 
-    if keys_pressed[pygame.K_s]: # and tetri.max_y + SQUARE_SIZE <= GAME_SIZE[1] - SQUARE_SIZE:
+    if keys_pressed[pygame.K_s] and tetri.max_y + SQUARE_SIZE <= GAME_SIZE[1] - SQUARE_SIZE * tetri.height:
         tetri.move_down()
 
     if keys_pressed[pygame.K_w] and tetri.min_y - SQUARE_SIZE >= 0:
-        pass
+        tetri.move_up()
+
+    if keys_pressed[pygame.K_r]:
+        tetri.rotate()
 
 
 
